0db415f366ca690cb12372b0296e0b68d6b9c17d
Areesha06 <areeshakashif287@gmail.com>
2026-08-07 14:09:01 +0500

    Setup Express server with ES modules

--- aninotes/backend/src/app.js
+++ aninotes/backend/src/app.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const cors = require('cors');
+import express from "express";
+import cors from "cors";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Notes App API is running');
+app.get("/", (req, res) => {
+  res.send("Notes App API is running");
 });
 
-module.exports = app;
+export default app;
